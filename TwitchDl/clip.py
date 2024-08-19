26ff25e3e756e8020a42cb57b42aa71b891b8d4f
atomicwrite <136755106+atomicwrite@users.noreply.github.com>
2024-08-19 09:25:25 -0500

    Update clip.py

--- TwitchDl/clip.py
+++ TwitchDl/clip.py
@@ -3,10 +3,9 @@
 from twitchdl.commands.download import get_clip_authenticated_url
 
 if __name__ == "__main__":
-    # print( 'https://dqrpb9wgowsf5.cloudfront.net/516df6c226be6b961dea_godofbronze5_44419492027_1719185184/chunked/index-dvr.m3u8')
+ 
     url = get_clip_authenticated_url(sys.argv[1], "source")
     print("_start_")
-    #print('C:\\twitchvods\\vid.mkv')
-    #print( 'https://dqrpb9wgowsf5.cloudfront.net/753b086b161dfc59cea0_godofbronze5_44429892251_1719356492/chunked/index-dvr.m3u8')
+ 
     print(url)
     print("_stop_")
